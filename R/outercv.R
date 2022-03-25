6d12392d73b8e3ded8f15c79debacd607fd1c887
Myles Lewis <myles.lewis@qmul.ac.uk>
2022-03-25 12:47:44 +0000

    Update outercv.formula

--- R/outercv.R
+++ R/outercv.R
@@ -3,18 +3,21 @@
 
 #' Outer cross-validation of selected models
 #'
-#' This is a convenience function designed to quickly evaluate performance of
-#' specific models (random forest, naive Bayes) with fixed hyperparameters and
-#' no tuning. If tuning of parameters on data is required, full nested CV with
-#' inner CV is recommended to tune model hyperparameters (see [nestcv.train]).
+#' This is a convenience function designed to use a single loop of
+#' cross-validation to quickly evaluate performance of specific models (random
+#' forest, naive Bayes, lm, glm) with fixed hyperparameters and no tuning. If
+#' tuning of parameters on data is required, full nested CV with inner CV is
+#' needed to tune model hyperparameters (see [nestcv.train]).
 #'
 #' @param y Response vector
 #' @param x Matrix of predictors
-#' @param model String specifying function to fit. Currently supports
-#'   randomForest and naive_bayes.
+#' @param formula A formula describing the model to be fitted
+#' @param data A matrix or data frame containing variables in the model.
+#' @param model Model function to be fitted.
 #' @param filterFUN Filter function, e.g. [ttest_filter] or [relieff_filter].
 #'   Any function can be provided and is passed `y` and `x`. Must return a
-#'   character vector with names of filtered predictors.
+#'   character vector with names of filtered predictors. Not available if
+#'   `outercv` is called with a formula.
 #' @param filter_options List of additional arguments passed to the filter
 #'   function specified by `filterFUN`.
 #' @param outer_method String of either `"cv"` or `"LOOCV"` specifying whether
@@ -28,6 +31,16 @@
 #'   is to use [nestcv.train] with `tuneGrid` set as a single row of a
 #'   data.frame. The parameters which are needed for a specific model can be
 #'   identified using [caret::modelLookup()].
+#'
+#'   The formula method for `outercv` is not recommended with large data sets -
+#'   it is designed primarily for more basic models e.g. `lm()` and `glm()`.
+#'   Filtering is not available if `outercv` is called with a formula - use `x`
+#'   and `y` interface instead.
+#'
+#'   Note that in the case of `model = lm`, although additional arguments e.g.
+#'   `subset`, `weights`, `offset` are passed into the model function via
+#'   `"..."` the scoping is known to go wrong. Avoid using these arguments with
+#'   `model = lm`.
 #' @importFrom caret createFolds confusionMatrix defaultSummary
 #' @importFrom data.table rbindlist
 #' @importFrom methods formalArgs
@@ -46,12 +59,12 @@ outercv <- function(y, ...) {
 #' 
 outercv.default <- function(y, x,
                             model,
-                    filterFUN = NULL,
-                    filter_options = NULL,
-                    outer_method = c("cv", "LOOCV"),
-                    n_outer_folds = 10,
-                    cores = 1,
-                    ...) {
+                            filterFUN = NULL,
+                            filter_options = NULL,
+                            outer_method = c("cv", "LOOCV"),
+                            n_outer_folds = 10,
+                            cores = 1,
+                            ...) {
   reg <- !(is.factor(y) | is.character(y))  # y = regression
   outer_method <- match.arg(outer_method)
   outer_folds <- switch(outer_method,
@@ -142,25 +155,45 @@ outercv.default <- function(y, x,
 
 
 #' @rdname outercv
+#' @importFrom stats model.frame model.response reformulate terms
 #' @export
 #' 
 outercv.formula <- function(formula, data,
                             model,
-                            filterFUN = NULL,
-                            filter_options = NULL,
                             outer_method = c("cv", "LOOCV"),
                             n_outer_folds = 10,
-                            cores = 1,
-                            ...) {
-  reg <- !(is.factor(y) | is.character(y))  # y = regression
+                            cores = 1, ...) {
+  # if model does not use formula, then revert to outercv.default(x, y, ...)
+  if (!"formula" %in% formalArgs(model)) {
+    # sample code from randomForest.formula/ svm.formula/ train.formula
+    m <- match.call(expand.dots = FALSE)
+    names(m)[2] <- "formula"
+    if (is.matrix(eval(m$data, parent.frame())))
+      m$data <- as.data.frame(data)
+    # m$... <- NULL
+    # m$na.action <- na.action
+    m[[1]] <- as.name("model.frame")
+    m <- eval(m, parent.frame())
+    y <- model.response(m)
+    Terms <- attr(m, "terms")
+    attr(Terms, "intercept") <- 0
+    # attr(y, "na.action") <- attr(m, "na.action")
+    m <- model.frame(terms(reformulate(attributes(Terms)$term.labels)),
+                     data.frame(m))
+    out <- outercv.default(y, m, outer_method = outer_method, 
+                           n_outer_folds = n_outer_folds, cores = cores, ...)
+    return(out)
+  }
+  # for models designed for formula method
   outer_method <- match.arg(outer_method)
+  y <- data[, all.vars(formula[[2]])]
+  reg <- !(is.factor(y) | is.character(y))  # y = regression
   outer_folds <- switch(outer_method,
                         cv = createFolds(y, k = n_outer_folds),
                         LOOCV = 1:length(y))
   outer_res <- mclapply(1:length(outer_folds), function(i) {
     test <- outer_folds[[i]]
-    y <- data[, all.vars(formula[[2]])]
-    fit <- model(formula = formula, data = data[test, ], ...)
+    fit <- model(formula = formula, data = data, ...)
     # test on outer CV
     predy <- predict(fit, newdata = data[test, ])
     preds <- data.frame(predy=predy, testy=y[test])
