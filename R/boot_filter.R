564447aca88df20f0fa9144ad6650b1e955d5259
Myles Lewis <myles.lewis@qmul.ac.uk>
2022-09-05 20:16:27 +0100

    Update boot_filter docs

--- R/boot_filter.R
+++ R/boot_filter.R
@@ -1,6 +1,19 @@
 
-
-
+#' Bootstrap for filter functions
+#'
+#' Randomly samples predictors and averages the ranking to give an ensemble
+#' measure of predictor variable importance.
+#'
+#' @param y Response vector
+#' @param x Matrix of predictors
+#' @param filterFUN Filter function, e.g. [ttest_filter()].
+#' @param B Number of times to bootstrap
+#' @param type Type of vector returned. Default "index" returns indices, "full"
+#'   returns full output.
+#' @param ... Optional arguments passed to the function specified by `filterFUN`
+#' @return Integer vector of indices of filtered parameters (`type = "index"`)
+#'   or if `type = "full"` a matrix of rankings from each bootstrap is returned.
+#' @seealso [boot_ttest()]
 #' @export
 #' 
 boot_filter <- function(y, x, filterFUN, B = 50, type = "index", ...) {
@@ -16,7 +29,20 @@ boot_filter <- function(y, x, filterFUN, B = 50, type = "index", ...) {
 }
 
 
+#' Bootstrap t-test filter
+#'
+#' Randomly samples predictors and averages the ranking from [ttest_filter()] to
+#' give an ensemble measure of best predictors by bootstrapped t-test.
+#'
+#' @param y Response vector
+#' @param x Matrix of predictors
+#' @param B Number of times to bootstrap
+#' @param ... Optional arguments passed to [ttest_filter()]
+#' @return Integer vector of indices of filtered parameters (`type = "index"`)
+#'   or if `type = "full"` a matrix of rankings from each bootstrap is returned.
+#' @seealso [boot_filter()]
 #' @export
+#' 
 boot_ttest <- function(y, x, B = 50, ...) {
   boot_filter(y, x, ttest_filter, B=B, ...)
 }
