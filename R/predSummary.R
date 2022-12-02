599fb1ba65787a008759fd1ea2b0df5b685e59bf
Myles Lewis <myles.lewis@qmul.ac.uk>
2022-12-02 14:51:46 +0000

    Reorganise

---
+++ R/predSummary.R
@@ -0,0 +1,41 @@
+
+#' Summarise prediction performance metrics
+#' 
+#' Quick function to calculate performance metrics: accuracy and balanced
+#' accuracy for classification; ROC AUC for binary classification; RMSE for
+#' regression.
+#' 
+#' @param output data.frame with columns `testy` containing observed response
+#'   from test folds; `predy` predicted response; `predyp` (optional) predicted
+#'   probabilities for classification to calculate ROC AUC
+#' @return Vector containing accuracy and balanced accuracy for classification,
+#'   ROC AUC for binary classification, RMSE for regression.
+#' 
+#' @export
+predSummary <- function(output) {
+  if (is.character(output$testy)) {
+    output$testy <- factor(output$testy)
+  }
+  if (is.factor(output$testy)) {
+    if (is.character(output$predy)) {
+      output$predy <- factor(output$predy, levels=levels(output$testy))
+    }
+    cm <- table(output$predy, output$testy)
+    acc <- sum(diag(cm))/ sum(cm)
+    ccm <- caret::confusionMatrix(cm)
+    b_acc <- ccm$byClass[11]
+    if (nlevels(output$testy) == 2) {
+      outputroc <- pROC::roc(output$testy, output$predyp, direction = "<", 
+                             quiet = TRUE)
+      auc <- outputroc$auc
+      summary <- setNames(c(auc, acc, b_acc), c("AUC", "Accuracy", "Balanced accuracy"))
+    } else {
+      summary <- setNames(c(acc, b_acc), c("Accuracy", "Balanced accuracy"))
+    }
+  } else {
+    df <- data.frame(obs = output$testy, pred = output$predy)
+    summary <- caret::defaultSummary(df)
+  }
+  class(summary) <- "predSummary"
+  summary
+}
