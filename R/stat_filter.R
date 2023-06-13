18f9583c87834ecb9546080401229909a86ad413
Myles Lewis <myles.lewis@qmul.ac.uk>
2023-06-13 22:36:51 +0100

    stat_filter()

--- R/stat_filter.R
+++ R/stat_filter.R
@@ -1,10 +1,17 @@
 
-#' Univariate filter for binary classification
+#' Univariate filter for binary classification with mixed predictor datatypes
 #'
 #' Univariate statistic filter for dataframes of predictors with mixed numeric
 #' and categorical datatypes. Different statistical tests are used depending on
-#' the data type of predictors: t-test for continuous data, chi-squared test for
-#' categorical data.
+#' the data type of response vector and predictors:
+#' \describe{
+#'   \item{Binary class response: `bin_stat_filter()`}{t-test for continuous 
+#'   data, chi-squared test for categorical data}
+#'   \item{Multiclass response: `class_stat_filter()`}{one-way ANOVA for 
+#'   continuous data, chi-squared test for categorical data}
+#'   \item{Continuous response: `lm_stat_filter()`}{linear regression for 
+#'   continuous data and binary data, one-way ANOVA for categorical data}
+#' }
 #'
 #' @param y Response vector
 #' @param x Matrix or dataframe of predictors
@@ -28,8 +35,28 @@
 #' @return Integer vector of indices of filtered parameters (type = "index") or
 #'   character vector of names (type = "names") of filtered parameters in order
 #'   of t-test/chi-squared test p-value. If `type` is `"full"` full output is
-#'   returned containing a matrix of results from [Rfast::ttests] and a matrix
-#'   of [chisq.test()] results.
+#'   returned containing a dataframe of statistical results. If `type` is
+#'   `"list"` the output is returned as a list of 2 matrices containing
+#'   statistical results separated by continuous and categorical predictors.
+#' @details
+#' `stat_filter()` is a wrapper which calls `bin_stat_filter()`,
+#' `class_stat_filter()` or `lm_stat_filter()` depending on whether `y` is
+#' binary, multiclass or continuous respectively.
+#' 
+#' @export
+#' 
+stat_filter <- function(y, x, ...) {
+  if (is.factor(y) || is.character(y)) {
+    if (nlevels(factor(y)) == 2) {
+      bin_stat_filter(y, x, ...)
+    } else class_stat_filter(y, x, ...)
+  } else {
+    lm_stat_filter(y, x, ...)
+  }
+}
+
+
+#' @rdname stat_filter
 #' @importFrom stats chisq.test
 #' @export
 #'
@@ -49,7 +76,7 @@ bin_stat_filter <- function(y,
   }
   if (is.null(colnames(x))) colnames(x) <- seq_len(ncol(x))
   x1 <- as.matrix(x[, !factor_ind])
-  x2 <- x[, factor_ind]
+  x2 <- x[, factor_ind, drop = FALSE]
   yi1 <- as.numeric(y) == 1
   yi2 <- as.numeric(y) == 2
   res1 <- NULL
@@ -73,7 +100,7 @@ bin_stat_filter <- function(y,
              type, keep_factors = FALSE, factor_ind = NULL, ...)
 }
 
-#' @rdname bin_stat_filter
+#' @rdname stat_filter
 #' @export
 #'
 class_stat_filter <- function(y,
@@ -91,12 +118,13 @@ class_stat_filter <- function(y,
   }
   if (is.null(colnames(x))) colnames(x) <- seq_len(ncol(x))
   x1 <- as.matrix(x[, !factor_ind])
-  x2 <- x[, factor_ind]
+  x2 <- x[, factor_ind, drop = FALSE]
   y <- factor(y)
   res1 <- NULL
   if (ncol(x1) > 0) {
-    res1 <- Rfast::ftests(x1, y)
-    rownames(res1) <- colnames(x1)
+      res1 <- oneway.tests.y(y, x1)
+      # res1 <- Rfast::ftests(x1, y)  # inaccurate
+      # rownames(res1) <- colnames(x1)
   }
   res2 <- chisq.tests(x2, y)[, 1:2]
   if (type == "list") return(list("F-test" = res1, "chisq.test" = res2))
@@ -106,6 +134,46 @@ class_stat_filter <- function(y,
   rescomb[factor_ind, ] <- res2
   if (type == "full") {
     test <- ifelse(factor_ind, "chi-squared", "F-test")
+    test <- data.frame(test)
+    return(cbind(test, rescomb))
+  }
+  filter_end(rescomb[, "pvalue"],
+             x, force_vars, nfilter, p_cutoff, rsq_cutoff,
+             type, keep_factors = FALSE, factor_ind = NULL, ...)
+}
+
+
+#' @rdname stat_filter
+#' @export
+#'
+lm_stat_filter <- function(y,
+                           x,
+                           force_vars = NULL,
+                           nfilter = NULL,
+                           p_cutoff = 0.05,
+                           rsq_cutoff = NULL,
+                           type = c("index", "names", "full", "list"),
+                           ...) {
+  type <- match.arg(type)
+  factor_ind <- index_factor(x, convert_bin = TRUE)
+  if (sum(factor_ind) == 0) {
+    return(lm_filter(y, x, force_vars, nfilter, p_cutoff, rsq_cutoff, type, ...))
+  }
+  if (is.null(colnames(x))) colnames(x) <- seq_len(ncol(x))
+  x1 <- data.matrix(x[, !factor_ind])
+  x2 <- x[, factor_ind, drop = FALSE]
+  res1 <- NULL
+  if (ncol(x1) > 0) {
+    res1 <- lm_filter(y, x1, type = "full")
+  }
+  res2 <- oneway.tests(x2, y)
+  if (type == "list") return(list("lm" = res1, "oneway.test" = res2))
+  rescomb <- matrix(nrow = ncol(x), ncol = 2,
+                    dimnames = list(colnames(x), c("stat", "pvalue")))
+  rescomb[!factor_ind, ] <- res1[, 3:4]
+  rescomb[factor_ind, ] <- res2[, c(1, 4)]
+  if (type == "full") {
+    test <- ifelse(factor_ind, "F-test", "lm.t-test")
     test <- data.frame(test)
     return(cbind(test, rescomb))
   }
@@ -125,8 +193,37 @@ chisq.tests <- function(x, y) {
 }
 
 
-index_factor <- function(x) {
+index_factor <- function(x, convert_bin = FALSE) {
   if (is.matrix(x)) return(NULL)
-  !unlist(lapply(x, is.numeric))
+  if (convert_bin) {
+    num_ind <- unlist(lapply(x, function(i) {
+      is.numeric(i) || nlevels(factor(i)) <= 2
+    }))
+  } else {
+    num_ind <- unlist(lapply(x, is.numeric))
+  }
+  !num_ind
 }
 
+
+# apply x
+oneway.tests <- function(x, y) {
+  res <- t(apply(x, 2, function(xi) {
+    fit <- suppressWarnings(oneway.test(y ~ xi))
+    unlist(fit[1:3])
+  }))
+  colnames(res) <- c("stat", "num df", "denom df", "pvalue")
+  res
+}
+
+
+# apply y
+# Rfast::ftests inaccurate
+oneway.tests.y <- function(x, y) {
+  res <- t(apply(y, 2, function(yi) {
+    fit <- suppressWarnings(oneway.test(yi ~ x))
+    unlist(fit[1:3])
+  }))
+  colnames(res) <- c("stat", "num df", "denom df", "pvalue")
+  res
+}
