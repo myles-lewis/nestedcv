f582388464757d7bf6943f8cfb897fa49063e020
Myles Lewis <myles.lewis@qmul.ac.uk>
2023-06-13 19:44:14 +0100

    class_stat_filter

--- R/stat_filter.R
+++ R/stat_filter.R
@@ -20,7 +20,9 @@
 #'   predictor by t-test is retained, while the other collinear predictors are
 #'   removed. See [collinear()].
 #' @param type Type of vector returned. Default "index" returns indices, "names"
-#'   returns predictor names, "full" returns a list of p values.
+#'   returns predictor names, "full" returns a matrix of statistics, "list"
+#'   returns a list of 2 matrices of statistics, one for t-test, one for
+#'   chi-squared test.
 #' @param ... optional arguments, e.g. `rsq_method`: see [collinear()].
 #'
 #' @return Integer vector of indices of filtered parameters (type = "index") or
@@ -37,7 +39,7 @@ bin_stat_filter <- function(y,
                             nfilter = NULL,
                             p_cutoff = 0.05,
                             rsq_cutoff = NULL,
-                            type = c("index", "names", "full"),
+                            type = c("index", "names", "full", "list"),
                             ...) {
   type <- match.arg(type)
   if (length(unique(y)) != 2) stop("y is not binary")
@@ -56,11 +58,57 @@ bin_stat_filter <- function(y,
     rownames(res1) <- colnames(x1)
   }
   res2 <- chisq.tests(x2, y)
-  if (type == "full") return(list("t.test" = res1, "chisq.test" = res2))
+  if (type == "list") return(list("t.test" = res1, "chisq.test" = res2))
   rescomb <- matrix(nrow = ncol(x), ncol = 3,
                     dimnames = list(colnames(x), colnames(res2)))
   rescomb[!factor_ind, ] <- res1
   rescomb[factor_ind, ] <- res2
+  if (type == "full") {
+    test <- ifelse(factor_ind, "chi-squared", "t-test")
+    test <- data.frame(test)
+    return(cbind(test, rescomb))
+  }
+  filter_end(rescomb[, "pvalue"],
+             x, force_vars, nfilter, p_cutoff, rsq_cutoff,
+             type, keep_factors = FALSE, factor_ind = NULL, ...)
+}
+
+#' @rdname bin_stat_filter
+#' @export
+#'
+class_stat_filter <- function(y,
+                              x,
+                              force_vars = NULL,
+                              nfilter = NULL,
+                              p_cutoff = 0.05,
+                              rsq_cutoff = NULL,
+                              type = c("index", "names", "full", "list"),
+                              ...) {
+  type <- match.arg(type)
+  factor_ind <- index_factor(x)
+  if (sum(factor_ind) == 0) {
+    return(anova_filter(y, x, force_vars, nfilter, p_cutoff, rsq_cutoff, type, ...))
+  }
+  if (is.null(colnames(x))) colnames(x) <- seq_len(ncol(x))
+  x1 <- as.matrix(x[, !factor_ind])
+  x2 <- x[, factor_ind]
+  y <- factor(y)
+  res1 <- NULL
+  if (ncol(x1) > 0) {
+    res1 <- Rfast::ftests(x1, y)
+    rownames(res1) <- colnames(x1)
+  }
+  res2 <- chisq.tests(x2, y)[, 1:2]
+  if (type == "list") return(list("F-test" = res1, "chisq.test" = res2))
+  rescomb <- matrix(nrow = ncol(x), ncol = 2,
+                    dimnames = list(colnames(x), colnames(res2)))
+  rescomb[!factor_ind, ] <- res1
+  rescomb[factor_ind, ] <- res2
+  if (type == "full") {
+    test <- ifelse(factor_ind, "chi-squared", "F-test")
+    test <- data.frame(test)
+    return(cbind(test, rescomb))
+  }
   filter_end(rescomb[, "pvalue"],
              x, force_vars, nfilter, p_cutoff, rsq_cutoff,
              type, keep_factors = FALSE, factor_ind = NULL, ...)
