29dad99077b1a607f5f8800abf672d0775ab8787
Myles Lewis <myles.lewis@qmul.ac.uk>
2023-06-30 12:56:17 +0100

    Add documentation

--- R/one_hot.R
+++ R/one_hot.R
@@ -1,5 +1,23 @@
 
-
+#' One-hot encode
+#' 
+#' One-hot encoding of factors in dataframes. Binary factors are converted to a
+#' single column of 0 and 1. Multi-level unordered factors are converted to
+#' multiple columns, one for each level. Unused levels are dropped. Numeric or
+#' integer columns are left untouched.
+#' 
+#' @param x A dataframe or matrix. Matrices are returned untouched.
+#' @param sep Character for separating factor variable names and levels when new
+#'   columns are generated.
+#' @return A numeric matrix with multi-level factors converted to one-hot
+#'   encoded extra columns with 0 and 1. Binary factors are converted to single
+#'   columns of 0 and 1. Ordered factors are converted to integer levels.
+#' @seealso [caret::dummyVars()]
+#' @examples
+#' data(iris)
+#' x <- iris
+#' one_hot(x)
+#' 
 #' @export
 #' 
 one_hot <- function(x, sep = ".") {
@@ -16,7 +34,7 @@ one_hot <- function(x, sep = ".") {
   }
   x0 <- data.matrix(x)
   x0[, bin_ind] <- x0[, bin_ind] - 1
-  x1 <- x0[, !factor_ind]
+  x1 <- x0[, !factor_ind, drop = FALSE]
   f <- which(factor_ind)
   expand_x2 <- lapply(f, function(i) {
     lev <- levels(factor(x[, i]))
