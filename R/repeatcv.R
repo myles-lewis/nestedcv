2ea9d12f7071dfb931d28aca126432ca6cc34cf8
Myles Lewis <myles.lewis@qmul.ac.uk>
2023-11-29 16:46:33 +0000

    Save repeat outer CV output

--- R/repeatcv.R
+++ R/repeatcv.R
@@ -9,13 +9,17 @@
 #' @param n Number of repeats
 #' @param repeat_folds Optional list containing fold indices to be applied to
 #'   the outer CV folds.
+#' @param keep Logical whether to save repeated outer CV predictions for ROC
+#'   curves etc.
 #' @param progress Logical whether to show progress.
 #' @details
 #' When comparing models, it is recommended to fix the sets of outer CV folds
 #' used across each repeat for comparing performance between models. The
 #' function [repeatfolds()] can be used to create a fixed set of outer CV folds
 #' for each repeat.
-#' @returns Matrix of performance metrics of S3 class 'repeatcv'.
+#' @returns List of S3 class 'repeatcv' containing the model call, matrix of
+#'   performance metrics, and if `keep = TRUE` a matrix or dataframe containing
+#'   the outer CV predictions from each repeat.
 #' @importFrom magrittr pipe_nested
 #' @importFrom utils setTxtProgressBar txtProgressBar
 #' @examples
@@ -48,7 +52,7 @@
 #' }
 #' @export
 
-repeatcv <- function(expr, n = 5, repeat_folds = NULL, progress = TRUE) {
+repeatcv <- function(expr, n = 5, repeat_folds = NULL, keep = FALSE, progress = TRUE) {
   cl <- match.call()
   if (!is.null(repeat_folds) && length(repeat_folds) != n)
     stop("mismatch between n and repeat_folds")
@@ -61,24 +65,35 @@ repeatcv <- function(expr, n = 5, repeat_folds = NULL, progress = TRUE) {
   if (d == "nestcv.train") d <- ex$method
   d <- gsub("nestcv.", "", d)
   if (progress) pb <- txtProgressBar2(title = d)
-  out <- lapply(seq_len(n), function(i) {
+  res <- lapply(seq_len(n), function(i) {
     if (!is.null(repeat_folds)) ex$outer_folds <- repeat_folds[[i]]
     fit <- try(eval.parent(ex), silent = TRUE)
     if (progress) setTxtProgressBar(pb, i / n)
     if (inherits(fit, "try-error")) {
       if (progress) warning(fit[1])
+      if (keep) return(list(NA, NA))
       return(NA)
     }
     s <- fit$summary
-    if (is.list(s)) return(s[[2]])
+    if (is.list(s)) s <- s[[2]]
+    if (keep) return(list(s, fit$output))
     s
   })
-  
   if (progress) close(pb)
-  out <- do.call(rbind, out)
-  rownames(out) <- seq_len(n)
-  attr(out, "call") <- ex0
-  class(out) <- c("repeatcv", class(out))
+  
+  if (keep) {
+    res1 <- lapply(res, "[[", 1)
+    result <- do.call(rbind, res1)
+    rownames(result) <- seq_len(n)
+    res2 <- lapply(res, "[[", 2)
+    output <- do.call(rbind, res2)
+    out <- list(call = ex0, result = result, output = output)
+  } else {
+    result <- do.call(rbind, res)
+    rownames(result) <- seq_len(n)
+    out <- list(call = ex0, result = result)
+  }
+  class(out) <- c("repeatcv")
   out
 }
 
@@ -118,23 +133,15 @@ repeatfolds <- function(y, repeats = 5, n_outer_folds = 10) {
 
 
 #' @export
-print.repeatcv <- function(x, ...) {
-  class(x) <- class(x)[class(x) != "repeatcv"]
-  attr(x, "call") <- NULL
-  print(x)
-}
-
-
-#' @export
 summary.repeatcv <- function(object,
                              digits = max(3L, getOption("digits") - 3L),
                              ...) {
   cat("Call:\n")
-  print(attr(object, "call"))
-  cat(nrow(object), "repeats\n")
-  m <- colMeans(object, na.rm = TRUE)
-  sd <- apply(object, 2, sd, na.rm = TRUE)
-  sem <- sd / sqrt(nrow(object))
+  print(object$call)
+  cat(nrow(object$result), "repeats\n")
+  m <- colMeans(object$result, na.rm = TRUE)
+  sd <- apply(object$result, 2, sd, na.rm = TRUE)
+  sem <- sd / sqrt(nrow(object$result))
   df <- data.frame(mean = m, sd = sd, sem = sem)
   print(df, digits = digits)
   invisible(df)
